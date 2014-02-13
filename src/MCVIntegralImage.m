33169a53da2c096ff95323a2ad8be21e85c6ede0
Yohsuke Yukishita <ykstyhsk@gmail.com>
2014-02-13 15:43:53 +0900

    [INTEGRAL] Support RGBA format

--- src/MCVIntegralImage.m
+++ src/MCVIntegralImage.m
@@ -12,48 +12,77 @@
     return obj;
 }
 
-- (BOOL)process:(MCVBufferFreight *)src to:(MCVBufferFreight *)dst
+static inline void __process_luminance(const uint8_t * restrict src,
+                                       GLhalf * restrict dst,
+                                       const int w, const int h)
 {
-    const uint8_t *luminance = [src.plane lockReadonly];
-    GLhalf *integral = [dst.plane lockWritable];
-
-    const int w = dst.plane.size.width;
-    const int h = dst.plane.size.height;
     uint32_t prev_integrals[w];
-    //uint32_t accum = 0;
 
-    /*
+    const float area = (float)((w*h)/4);
+
     for (int i = 0; i < w; ++i) {
-        *integral++ = 0.0f;
-        ++luminance;
-    }
-     */
-    for (int i = 0; i < w; ++i) {
-        integral[i] = 0;
+        dst[i] = 0;
         prev_integrals[i] = 0;//luminance[i];
     }
+
     for (int i = 1; i < h; ++i) {
         uint32_t accum = 0;
         for (int j = 0; j < w; ++j) {
-            accum += luminance[i * w + j];
+            accum += src[i * w + j];
             uint32_t prev_integral = prev_integrals[j];
             prev_integrals[j] = accum + prev_integral;
-            integral[i * w + j] = convertFloatToHFloat((accum + prev_integral) / (float)(w*h/4));
-            //integral[i * w + j] = ((accum + prev_integral) / (float)(w*h));
+            dst[i * w + j] = convertFloatToHFloat((accum + prev_integral) / area);
         }
+    }
+}
+
+static inline void __process_rgba(const uint8_t * restrict src,
+                                       GLhalf * restrict dst,
+                                       const int w, const int h)
+{
+    uint32_t prev_integrals[w];
+
+    const float area = (float)((w*h)/4);
+
+    for (int i = 0; i < w; ++i) {
+        dst[i] = 0;
+        prev_integrals[i] = 0;//luminance[i];
     }
 
-    //memcpy(integral, integral, w *h * sizeof(float));
-    /*
-    for (int i = w; i < w*h; ++i) {
-        if (i % w == 0) {
-            accum = 0;
+    // process red component only
+    for (int i = 1; i < h; ++i) {
+        uint32_t accum = 0;
+        for (int j = 0; j < w; ++j) {
+            accum += src[i * (w * 4) + (j * 4)];
+            uint32_t prev_integral = prev_integrals[j];
+            prev_integrals[j] = accum + prev_integral;
+            dst[i * w + j] = convertFloatToHFloat((accum + prev_integral) / area);
         }
-        accum += *luminance++;
-        *integral = (accum + *(integral - w)) / (GLfloat)(w*h);
-        ++integral;
     }
-     */
+}
+
+- (BOOL)process:(MCVBufferFreight *)src to:(MCVBufferFreight *)dst
+{
+    NSASSERT(dst.internal_format == GL_LUMINANCE16F_EXT);
+
+    const void *src_buf = [src.plane lockReadonly];
+    GLhalf *dst_buf = [dst.plane lockWritable];
+
+    const int w = dst.plane.size.width;
+    const int h = dst.plane.size.height;
+
+    BENCHMARK("integral")
+    switch (src.internal_format) {
+        case GL_LUMINANCE:
+            __process_luminance(src_buf, dst_buf, w, h);
+            break;
+        case GL_RGBA:
+            __process_rgba(src_buf, dst_buf, w, h);
+            break;
+        default:
+            NSASSERT(!"Illegal format");
+            break;
+    }
 
     [src.plane unlockReadonly];
     [dst.plane unlockWritable];
